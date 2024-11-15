d04b5d252018908594034574455c2313e9623c8e
maipants <tkrkh43@gmail.com>
2024-11-14 20:07:24 -0500

    Add project files.

---
+++ mainPackage/main.py
@@ -0,0 +1,17 @@
+# Name: Sarah Mahan, Drew Mehlman
+# email:  mahansa@mail.uc.edu, mehlmadm@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date: 11/15/24 (Extension)
+# Course #/Section: IS4010 001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment:  
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations:
+# Anything else that's relevant:
+
+
+#main.py
+
+if __name__ == "__main__":
+    None
