e792fa15d2d215531c726adb36b68a9ff54facff
maipants <tkrkh43@gmail.com>
2024-11-14 20:16:59 -0500

    Imported slide info

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -13,5 +13,47 @@
 
 #main.py
 
+import json
+import requests
+
+json_string = """
+{
+"researcher": {
+"name": "Ford Prefect",
+"species": "Betelgeusian",
+"relatives": [
+{
+"name": "Zaphod Beeblebrox",
+"species": "Betelgeusian"
+}
+]
+}
+}
+"""
+data = json.loads(json_string)  
+
+response = requests.get('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=pfJKDXPzTykVL73ehnPyY8pkDQLjfq5cz5LqCkl3')
+json_string = response.content
+parsed_json = json.loads(json_string) # Now we have a python dictionary
+#print(parsed_json)
+#print(parsed_json['data'][0]['description'])
+#print(parsed_json['data'][0]['directionsInfo'])
+total = int(parsed_json['total']) # The number of parks that were returned
+for park in parsed_json['data']:
+    print(park)
+
+def iterate_dictionary(myDictionary):
+    for k, v in myDictionary.items():
+        print("key is " + str(type(k)) + ", value is " + str(type(v)))
+        if isinstance(v, dict):
+            iterate_dictionary(v)
+        else:
+            print("{0} : {1}".format(k, v))
+            if (isinstance(v, list)):
+                for vv in v:
+                    if (isinstance(vv, dict)):
+                        iterate_dictionary(vv)
+
 if __name__ == "__main__":
     None
+ 
